f23bea71a49cac0e0e34846306d133aecb4cdc29
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-03 15:40:39 +0100

    unprotected

---
+++ src/main/kotlin/no/nav/aap/rest/UnprotectedRestController.kt
@@ -0,0 +1,21 @@
+package no.nav.aap.rest
+
+import no.nav.boot.conditionals.ConditionalOnNotProd
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.lang.annotation.Documented
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.TYPE
+
+
+@RestController
+@RequestMapping
+@Unprotected
+@Documented
+@ConditionalOnNotProd
+@Target(TYPE)
+@Retention(RUNTIME)
+annotation class UnprotectedRestController(vararg val value: String = [],
+                                           val produces: Array<String> = [APPLICATION_JSON_VALUE])
