412e0ef542171fb8b76296942a69a8c8ce9e0c40
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-30 13:18:42 +0200

    tokens also

--- src/main/kotlin/no/nav/aap/rest/MDCPropagatingFilterFunction.kt
+++ src/main/kotlin/no/nav/aap/rest/MDCPropagatingFilterFunction.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.rest
 
 import no.nav.aap.util.LoggerUtil
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import org.slf4j.MDC
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ClientResponse
@@ -8,15 +9,17 @@ import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeFunction
 import reactor.core.publisher.Mono
 
-class MDCPropagatingFilterFunction : ExchangeFilterFunction {
+class MDCPropagatingFilterFunction(private val holder: TokenValidationContextHolder) : ExchangeFilterFunction {
     private val log = LoggerUtil.getLogger(javaClass)
     override fun filter(request: ClientRequest, next: ExchangeFunction): Mono<ClientResponse> {
             val map  = MDC.getCopyOfContextMap()
+            val current = holder.tokenValidationContext
             return next.exchange(request)
                 .doOnNext {
                     if (map != null) {
-                        log.trace("Setter  map fo neste tråd")
+                        log.trace("Setter map og token for neste")
                         MDC.setContextMap(map)
+                        holder.tokenValidationContext = current
                     }
                 }
     }
