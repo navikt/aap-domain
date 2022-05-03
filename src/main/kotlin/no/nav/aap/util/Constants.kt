e70a19075601805ef182dcbd2c700effacb13c74
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-03 11:38:25 +0200

    lowercase

--- src/main/kotlin/no/nav/aap/util/Constants.kt
+++ src/main/kotlin/no/nav/aap/util/Constants.kt
@@ -1,13 +1,13 @@
 package no.nav.aap.util
 
 object Constants {
-    const val JOARK = "JOARK"
+    const val JOARK = "joark"
     const val INNTEKTSKOMPONENT = "INNTEKTSKOMPONENT"
-    const val PDL_USER = "PDL"
-    const val PDL_SYSTEM = "PDLS"
-    const val STS = "STS"
-    const val TEMA = "TEMA"
-    const val AAP = "AAP"
+    const val PDL_USER = "pdl"
+    const val PDL_SYSTEM = "pdls"
+    const val STS = "sts"
+    const val TEMA = "tema"
+    const val AAP = "aap"
     const val NAV_CONSUMER_TOKEN = "Nav-Consumer-Token"
     const val NAV_CONSUMER_ID = "Nav-Consumer-Id"
     const val IDPORTEN = "idporten"
