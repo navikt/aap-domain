6e66c58ca1ad708590e547d529184cb8586da37f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-12 22:04:27 +0200

    name ctr arg

--- src/main/kotlin/no/nav/aap/rest/AbstractRestConfig.kt
+++ src/main/kotlin/no/nav/aap/rest/AbstractRestConfig.kt
@@ -3,8 +3,7 @@ package no.nav.aap.rest
 import no.nav.aap.util.URIUtil
 import java.net.URI
 
-abstract class AbstractRestConfig(val baseUri: URI, protected val pingPath: String, val isEnabled: Boolean) {
+abstract class AbstractRestConfig(val baseUri: URI, protected val pingPath: String,val  name: String = baseUri.host, val isEnabled: Boolean) {
     val pingEndpoint = URIUtil.uri(baseUri, pingPath)
-    val name = baseUri.host
     override fun toString() = "${javaClass.simpleName} [pingPath=$pingPath,enabled=$isEnabled,baseUri=$baseUri]"
 }
