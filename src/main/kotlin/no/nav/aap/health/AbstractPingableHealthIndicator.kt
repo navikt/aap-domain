2f6a6ac3c1a05a7071802aace26cfe1d64582946
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-23 14:23:44 +0200

    url

--- src/main/kotlin/no/nav/aap/health/AbstractPingableHealthIndicator.kt
+++ src/main/kotlin/no/nav/aap/health/AbstractPingableHealthIndicator.kt
@@ -13,7 +13,7 @@ abstract class AbstractPingableHealthIndicator(private val pingable: Pingable) :
             down(e)
         }
 
-    private fun up() = with(pingable) { Health.up().withDetail(name(), pingEndpoint()).build() }
+    private fun up() = with(pingable) { Health.up().withDetail("url", pingEndpoint()).build() }
     private fun down(e: Exception)  = with(pingable) { down().withDetail("url", pingEndpoint()).withException(e).build() }
 
     override fun toString() = "${javaClass.simpleName} [pingable=$pingable]"
