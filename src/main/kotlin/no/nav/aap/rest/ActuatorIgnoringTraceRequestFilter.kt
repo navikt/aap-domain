3c35047df46621b867c5917c062087ce1ae40601
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-18 10:48:07 +0200

    bump boot

--- src/main/kotlin/no/nav/aap/rest/ActuatorIgnoringTraceRequestFilter.kt
+++ src/main/kotlin/no/nav/aap/rest/ActuatorIgnoringTraceRequestFilter.kt
@@ -1,10 +1,10 @@
 package no.nav.aap.rest
 
-import jakarta.servlet.ServletException
-import jakarta.servlet.http.HttpServletRequest
 import org.springframework.boot.actuate.trace.http.HttpExchangeTracer
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository
 import org.springframework.boot.actuate.web.trace.servlet.HttpTraceFilter
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
 
 class ActuatorIgnoringTraceRequestFilter(repository: HttpTraceRepository?, tracer: HttpExchangeTracer?) :
     HttpTraceFilter(repository, tracer) {
