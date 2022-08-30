d90dc007e671d269174c3e2070afb85d6b6a635b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-30 11:29:23 +0200

    ettersending skjematyper

--- src/main/kotlin/no/nav/aap/api/felles/Søker.kt
+++ src/main/kotlin/no/nav/aap/api/felles/Søker.kt
@@ -47,5 +47,8 @@ data class Adresse (val adressenavn: String?, val husbokstav: String?, val husnu
 
 enum class SkjemaType(val kode: String, val tittel: String) {
     UTLAND("NAV 11-03.07", "søknad om å beholde AAP ved opphold i utlandet"),
-    STANDARD("NAV 11-13.05", "søknad om AAP")
+    UTLAND_ETTERSENDING("NAVe 11-03.07", "settersending til ${UTLAND.tittel}"),
+    STANDARD("NAV 11-13.05", "søknad om AAP"),
+    STANDARD_ETTERSENDING("NAVe 11-13.05", "ettersending til ${STANDARD.tittel}")
+
 }
