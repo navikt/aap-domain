ec9593822af902ba2ca4ee19b186f62f3554438d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-03 08:55:15 +0200

    rename

--- src/main/kotlin/no/nav/aap/api/felles/Søker.kt
+++ src/main/kotlin/no/nav/aap/api/felles/Søker.kt
@@ -48,8 +48,8 @@ data class Adresse (val adressenavn: String?, val husbokstav: String?, val husnu
 
 
 enum class SkjemaType(val kode: String, val tittel: String) {
-    UTLAND("NAV 11-03.07", "Søknad om å beholde AAP ved opphold i utlandet"),
-    UTLAND_ETTERSENDING("NAVe 11-03.07", "ettersending til ${UTLAND.tittel.decap()}"),
+    UTLAND_SØKNAD("NAV 11-03.07", "Søknad om å beholde AAP ved opphold i utlandet"),
+    UTLAND_ETTERSENDING("NAVe 11-03.07", "ettersending til ${UTLAND_SØKNAD.tittel.decap()}"),
     STANDARD("NAV 11-13.05", "Søknad om AAP"),
     STANDARD_ETTERSENDING("NAVe 11-13.05", "ettersending til ${STANDARD.tittel.decap()}")
 
