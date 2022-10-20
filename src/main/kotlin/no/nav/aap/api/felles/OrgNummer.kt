54713f325f0b02efcf205beb516f16d16c601910
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-20 10:26:52 +0200

    orgnr validering

--- src/main/kotlin/no/nav/aap/api/felles/OrgNummer.kt
+++ src/main/kotlin/no/nav/aap/api/felles/OrgNummer.kt
@@ -1,14 +1,27 @@
 package no.nav.aap.api.felles
 
 import com.fasterxml.jackson.annotation.JsonValue
+import no.nav.aap.util.LoggerUtil
+import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.boot.conditionals.Cluster.currentCluster
 import no.nav.boot.conditionals.EnvUtil.DEV_GCP
+import org.apache.commons.lang3.StringUtils.substring
+import org.slf4j.Logger
 
 data class OrgNummer(@get:JsonValue val orgnr: String) {
 
-   init {
+    protected val log: Logger = getLogger(javaClass)
+
+    init {
        if (!currentCluster().equals(DEV_GCP)) {
-           require(isValid(orgnr)) { "Ugyldig organisasjonsnummer $orgnr" }
+           log.trace("Vi er i cluster ${currentCluster()}, gjør validering av $orgnr")
+           require(orgnr.length == 9) { "Orgnr må ha lengde 9, $orgnr har lengde ${orgnr.length} "}
+           require(orgnr.startsWith("8") || orgnr.startsWith("9")) { "Orgnr må begynne med 8 eller 9"}
+           require(orgnr[8].code - 48 == mod11(orgnr.substring(0, 8))) {"${orgnr[8]} feilet mod11 validering"}
+           //require(isValid(orgnr)) { "Ugyldig organisasjonsnummer $orgnr, må være 9 tegn lang og begynne med 8 eller 9 og validere mod11" }
+       }
+        else {
+            log.trace("Vi er i cluster ${currentCluster()}, ingen validering av $orgnr")
        }
     }
     companion object {
