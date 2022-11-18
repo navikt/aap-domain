e92db0c0a89c9ac1491c9b899b349db367945b8f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-18 21:58:51 +0100

    object

--- src/main/kotlin/no/nav/aap/util/TokenExtensions.kt
+++ src/main/kotlin/no/nav/aap/util/TokenExtensions.kt
@@ -5,7 +5,7 @@ import no.nav.aap.util.StringExtensions.asBearer
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 
-class TokenExtensions {
+object TokenExtensions {
 
      fun OAuth2AccessTokenService.bearerToken(props: ClientProperties?, url: URI) =
          props?.let {
