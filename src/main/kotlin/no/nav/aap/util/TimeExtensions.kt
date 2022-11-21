88f90d28415438d5c35fb474624745249d661d3b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-21 16:39:29 +0100

    UTC

--- src/main/kotlin/no/nav/aap/util/TimeExtensions.kt
+++ src/main/kotlin/no/nav/aap/util/TimeExtensions.kt
@@ -3,15 +3,14 @@ package no.nav.aap.util
 import java.time.Instant.ofEpochMilli
 import java.time.LocalDateTime
 import java.time.LocalDateTime.ofInstant
+import java.time.ZoneId.of
 import java.time.ZoneId.systemDefault
+import java.time.ZoneOffset.UTC
 import java.time.format.DateTimeFormatter.ofPattern
-import java.util.Date
 
 object TimeExtensions {
     fun Long.format(fmt: String = "yyyy-MM-dd HH:mm:ss") =  ofInstant(ofEpochMilli(this), systemDefault()).format(ofPattern(fmt))
 
-    fun Date.toLocalDateTime(): LocalDateTime =
-        ofEpochMilli(time)
-            .atZone(systemDefault())
-            .toLocalDateTime()
+     fun LocalDateTime.toUTC(): LocalDateTime = atZone(of("Europe/Oslo")).withZoneSameInstant(UTC).toLocalDateTime()
+
 }
