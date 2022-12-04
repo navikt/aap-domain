16805c6a003b45b027990474659d1070e17ec5c1
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-12-04 10:56:26 +0100

    flyttet retry config

--- src/main/kotlin/no/nav/aap/rest/AbstractRestConfig.kt
+++ src/main/kotlin/no/nav/aap/rest/AbstractRestConfig.kt
@@ -4,16 +4,28 @@ import no.nav.aap.util.URIUtil.uri
 import java.net.URI
 import java.time.Duration
 import org.slf4j.Logger
+import org.springframework.boot.context.properties.bind.DefaultValue
+import org.springframework.boot.convert.DurationStyle
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.WebClientResponseException.NotFound
 import org.springframework.web.reactive.function.client.WebClientResponseException.Unauthorized
 import reactor.util.retry.Retry
 
-abstract class AbstractRestConfig(val baseUri: URI, protected val pingPath: String, name: String = baseUri.host,  isEnabled: Boolean, val retries: Long, val delay: Duration) : AbstractConfig(name,isEnabled){
+abstract class AbstractRestConfig(val baseUri: URI, protected val pingPath: String, name: String = baseUri.host,  isEnabled: Boolean = true, val retry: RetryConfig = RetryConfig.DEFAULT) : AbstractConfig(name,isEnabled){
     val pingEndpoint = uri(baseUri, pingPath)
 
+
+    data class RetryConfig(@DefaultValue(DEFAULT_RETRIES)  val retries: Long,
+                           @DefaultValue(DEFAULT_DELAY)  val delayed: Duration) {
+        companion object {
+            const val DEFAULT_RETRIES = "3"
+            const val DEFAULT_DELAY = "100ms"
+            val DEFAULT = RetryConfig(DEFAULT_RETRIES.toLong(), DurationStyle.detectAndParse(DEFAULT_DELAY))
+        }
+    }
+
     fun retrySpec(log: Logger) =
-        Retry.fixedDelay(retries, delay)
+        Retry.fixedDelay(retry.retries, retry.delayed)
             .filter { e -> e is WebClientResponseException && e !is Unauthorized && e !is NotFound }
             .onRetryExhaustedThrow { _, s ->  s.failure()}
             .doBeforeRetry { s -> log.warn("Retry kall mot $baseUri grunnet exception ${s.failure().javaClass.name} og melding ${s.failure().message} for ${s.totalRetriesInARow() + 1} gang, prøver igjen") }
