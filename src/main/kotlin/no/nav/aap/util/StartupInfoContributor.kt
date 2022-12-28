73f9cb7babeb19f314d5756e5a6425aee5a4144d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-12-28 10:20:36 +0100

    Oslo

--- src/main/kotlin/no/nav/aap/util/StartupInfoContributor.kt
+++ src/main/kotlin/no/nav/aap/util/StartupInfoContributor.kt
@@ -1,5 +1,9 @@
 package no.nav.aap.util
 
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import no.nav.aap.util.TimeExtensions.format
 import org.springframework.boot.actuate.info.Info.Builder
 import org.springframework.boot.actuate.info.InfoContributor
@@ -7,6 +11,10 @@ import org.springframework.context.ApplicationContext
 
 class StartupInfoContributor(private val ctx: ApplicationContext) : InfoContributor {
     override fun contribute(builder: Builder) {
-        builder.withDetail("extra-info", mapOf("Startup time" to ctx.startupDate.format()))
+        builder.withDetail("extra-info", mapOf("Startup time" to ctx.startupDate.local()))
     }
+
+    private fun Long.local(fmt: String = "yyyy-MM-dd HH:mm:ss") =  LocalDateTime.ofInstant(Instant.ofEpochMilli(this),
+            ZoneId.of("Europe/Oslo")).format(DateTimeFormatter.ofPattern(fmt))
+
 }
