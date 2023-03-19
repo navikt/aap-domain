593606158739449723f87cb312bd070224733cc7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-19 10:52:54 +0100

    chaos monkey

--- src/main/kotlin/no/nav/aap/util/ChaosMonkey.kt
+++ src/main/kotlin/no/nav/aap/util/ChaosMonkey.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.util
 
 import no.nav.boot.conditionals.Cluster.Companion.currentCluster
+import org.checkerframework.checker.units.qual.t
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_GATEWAY
 import org.springframework.web.reactive.function.client.WebClientResponseException
@@ -8,13 +9,12 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 class ChaosMonkey(private val criteria: () -> Boolean) {
     private val log = LoggerUtil.getLogger(ChaosMonkey::class.java)
 
-    fun inhject(component: Any,status: HttpStatus = BAD_GATEWAY)  =
+    fun inhjectFault(component: Any,status: HttpStatus)  = injectFault(WebClientResponseException(status, "Tvunget feil  fra ${component.javaClass.simpleName}}", null, null, null, null))
+    private fun injectFault(t: Throwable) =
         if (criteria.invoke()) {
-            throw WebClientResponseException(status, "Tvunget feil i $currentCluster fra ${component.javaClass.simpleName}}", null, null, null, null).also {
+            throw t.also {
                 log.warn(it.message)
             }
         }
-        else {
-            Unit
-        }
+        else Unit
 }
