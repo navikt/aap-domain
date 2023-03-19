5e9bba005ad19baed2b02cd16780b72b73ecfd39
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-19 10:05:11 +0100

    flyttet monkey

--- src/main/kotlin/no/nav/aap/util/ChaosMonkey.kt
+++ src/main/kotlin/no/nav/aap/util/ChaosMonkey.kt
@@ -5,10 +5,10 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_GATEWAY
 import org.springframework.web.reactive.function.client.WebClientResponseException
 
-class ChaosMonkey(private val criteria: () -> Boolean, private val status: HttpStatus = BAD_GATEWAY) {
+class ChaosMonkey(private val criteria: () -> Boolean) {
     private val log = LoggerUtil.getLogger(ChaosMonkey::class.java)
 
-    fun inhject(component: Any)  =
+    fun inhject(component: Any,status: HttpStatus = BAD_GATEWAY)  =
         if (criteria.invoke()) {
             throw WebClientResponseException(status, "Tvunget feil i $currentCluster fra ${component.javaClass.simpleName}}", null, null, null, null).also {
                 log.warn(it.message)
