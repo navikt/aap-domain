4ebdc1701048374a4c55f9db394ffec4d733d11d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-20 15:10:59 +0100

    subclasses

--- src/main/kotlin/no/nav/aap/api/felles/error/IntegrationException.kt
+++ src/main/kotlin/no/nav/aap/api/felles/error/IntegrationException.kt
@@ -2,8 +2,8 @@ package no.nav.aap.api.felles.error
 
 import java.net.URI
 
-open class IntegrationException(msg: String?, uri: URI? = null, cause: Throwable? = null) : RuntimeException(msg, cause)
+abstract class IntegrationException(msg: String?, uri: URI? = null, cause: Throwable? = null) : RuntimeException(msg, cause)
 
-class RecoverableIntegrationException(msg: String?, uri: URI? = null, cause: Throwable? = null) : IntegrationException(msg,uri,cause)
+open class RecoverableIntegrationException(msg: String?, uri: URI? = null, cause: Throwable? = null) : IntegrationException(msg,uri,cause)
 
-class IrrecoverableIntegrationException(msg: String?, uri: URI? = null, cause: Throwable? = null) : IntegrationException(msg,uri,cause)
+open class IrrecoverableIntegrationException(msg: String?, uri: URI? = null, cause: Throwable? = null) : IntegrationException(msg,uri,cause)
