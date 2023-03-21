36e6148310422486b0e76fc6faf3706155e0fdfe
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-21 10:55:27 +0100

    chunked

--- src/main/kotlin/no/nav/aap/util/Metrics.kt
+++ src/main/kotlin/no/nav/aap/util/Metrics.kt
@@ -1,22 +1,14 @@
 package no.nav.aap.util
 
-import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Counter.*
 import io.micrometer.core.instrument.Metrics.globalRegistry
 
 object Metrikker {
 
-    fun inc(navn: String, vararg tags: Pair<String,Any>) =
-        tags(Counter.builder(navn),*tags)
-            .register(globalRegistry)
-            .increment()
-
-    fun inc(navn: String, vararg tags: String) = Counter.builder(navn)
-        .tags(*tags.map(String::lowercase).toTypedArray())
-        .register(globalRegistry)
-        .increment()
-
-    private fun tags(b: Counter.Builder, vararg pairs: Pair<String,Any>) =  pairs.iterator().forEachRemaining { tag(b, it) }.let { b }
-
-    private fun tag(b: Counter.Builder, t: Pair<String,Any>) = b.tags(t.first,"${t.second}")
-
+    fun inc(navn: String, vararg tags: String) =  inc(navn,tags.toList().chunked(2) { Pair(it[0], it[1])})
+    fun inc(navn: String,  tags: List<Pair<String,Any>>)  =
+        builder(navn).apply {
+            tags.forEach { tag(it.first, "${it.second}") }
+            register(globalRegistry).increment()
+        }
 }
