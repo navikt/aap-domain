112a67ca483f8c4a01c6b1bccaea40830f8d25a6
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-04-13 10:37:53 +0200

    spring versions

--- src/main/kotlin/no/nav/aap/util/StartupInfoContributor.kt
+++ src/main/kotlin/no/nav/aap/util/StartupInfoContributor.kt
@@ -4,14 +4,18 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import org.springframework.boot.SpringBootVersion
 import no.nav.aap.util.TimeExtensions.format
 import org.springframework.boot.actuate.info.Info.Builder
 import org.springframework.boot.actuate.info.InfoContributor
 import org.springframework.context.ApplicationContext
+import org.springframework.core.SpringVersion
 
 class StartupInfoContributor(private val ctx: ApplicationContext) : InfoContributor {
     override fun contribute(builder: Builder) {
-        builder.withDetail("extra-info", mapOf("Startup time" to ctx.startupDate.local()))
+        builder.withDetail("extra-info", mapOf("Startup time" to ctx.startupDate.local(),
+            "Spring Boot version" to SpringBootVersion.getVersion(),
+            "Spring Framework version" to SpringVersion.getVersion()))
     }
 
     private fun Long.local(fmt: String = "yyyy-MM-dd HH:mm:ss") =  LocalDateTime.ofInstant(Instant.ofEpochMilli(this),
