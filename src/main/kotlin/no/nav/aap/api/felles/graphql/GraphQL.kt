1e08c9d6b99a56220ccc97d9d5b118acb1414787
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-15 14:40:44 +0200

    chore

--- src/main/kotlin/no/nav/aap/api/felles/graphql/GraphQL.kt
+++ src/main/kotlin/no/nav/aap/api/felles/graphql/GraphQL.kt
@@ -103,8 +103,8 @@ abstract class AbstractGraphQLAdapter(client : WebClient, cfg : AbstractRestConf
                 .variables(vars)
                 .retrieve(query.second)
                 .toEntityList(T::class.java)
-                .retryWhen(retrySpec(log, "/") { it is RecoverableGraphQLException  || it is GraphQlTransportException})
                 .onErrorMap { if (it  is FieldAccessException)  it.oversett() else it}
+                .retryWhen(retrySpec(log, "/") { it is RecoverableGraphQLException  || it is GraphQlTransportException})
                 .contextCapture()
                 .block() ?: emptyList()).also {
                 log.trace("Slo opp liste av {} {}", T::class.java.simpleName, it)
@@ -121,8 +121,8 @@ abstract class AbstractGraphQLAdapter(client : WebClient, cfg : AbstractRestConf
                 .variables(vars)
                 .retrieve(query.second)
                 .toEntity(T::class.java)
-                .retryWhen(retrySpec(log, "/") { it is RecoverableGraphQLException  || it is GraphQlTransportException})
                 .onErrorMap { if (it  is FieldAccessException)  it.oversett() else it}
+                .retryWhen(retrySpec(log, "/") { it is RecoverableGraphQLException  || it is GraphQlTransportException})
                 .contextCapture()
                 .block().also {
                     log.trace("Slo opp {} {}", T::class.java.simpleName, it)
