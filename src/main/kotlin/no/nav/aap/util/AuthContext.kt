cc865a2dfa5a46cdfa86e68841697460be90983d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-06-01 08:59:44 +0200

    coroutine support

--- src/main/kotlin/no/nav/aap/util/AuthContext.kt
+++ src/main/kotlin/no/nav/aap/util/AuthContext.kt
@@ -1,5 +1,9 @@
 package no.nav.aap.util
 
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.ThreadContextElement
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.util.Constants.IDPORTEN
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
@@ -19,4 +23,22 @@ class AuthContext(private val ctxHolder : TokenValidationContextHolder) {
         ?.let {
             Fødselsnummer(it)
         } ?: throw JwtTokenMissingException("Intet token i context")
+}
+
+class RequestContextCoroutineContext(private val requestAttributes: RequestAttributes? = RequestContextHolder.getRequestAttributes()) : ThreadContextElement<RequestAttributes?> {
+    companion object Key : CoroutineContext.Key<RequestContextCoroutineContext>
+
+    override val key: CoroutineContext.Key<RequestContextCoroutineContext> get() = Key
+
+    override fun updateThreadContext(context: CoroutineContext): RequestAttributes? {
+        val previousAttributes =  RequestContextHolder.getRequestAttributes()
+        RequestContextHolder.setRequestAttributes(requestAttributes)
+        return previousAttributes
+    }
+
+    override fun restoreThreadContext(context: CoroutineContext, oldState: RequestAttributes?) {
+        oldState?.let {
+            RequestContextHolder.setRequestAttributes(oldState)
+        } ?: RequestContextHolder.resetRequestAttributes()
+    }
 }
